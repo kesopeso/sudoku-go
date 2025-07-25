21ec24280d158727d7112de3449371907ed40f27
Dorde Kesic <dordekesic.keso@gmail.com>
2025-07-25 14:09:55 +0200

    adds basic sudoku init with some fields validation, still some validation missing, a lot of refactoring needed

--- sudoku.go
+++ sudoku.go
@@ -1,18 +1,68 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+	"math"
+)
 
 type Sudoku struct {
-	Count int
+	Fields [][]int
 }
 
-func NewSudoku() *Sudoku {
-	return &Sudoku{
-		Count: 10,
+func NewSudoku(fields [][]int) (*Sudoku, error) {
+	if error := validateFields(fields); error != nil {
+		return nil, error
 	}
+	return &Sudoku{Fields: fields}, nil
+}
+
+func validateFields(fields [][]int) error {
+	// validate length of fields
+	fieldsCount := len(fields)
+	squareCount := math.Sqrt(float64(fieldsCount))
+	squareCountAsInt := int(squareCount)
+	if squareCount-float64(squareCountAsInt) != 0 {
+		return fmt.Errorf("fields array should be power of two, for instance 4, 9, 16, etc")
+	}
+
+	for _, rV := range fields {
+		// validate repeating row/column numbers
+
+		// validate row length
+		if len(rV) > fieldsCount {
+			return fmt.Errorf("too many elements in a row, found: %v, max: %v", len(rV), fieldsCount)
+		}
+		for _, cV := range rV {
+			// validate field min value
+			if cV < 0 {
+				return fmt.Errorf("field cannot be smaller than 0 (which represents empty field)")
+			}
+			// validate field max value
+			if cV > fieldsCount {
+				return fmt.Errorf("field cannot be bigger than %v", fieldsCount)
+			}
+		}
+	}
+
+	return nil
 }
 
 func main() {
-	sudoku := NewSudoku()
-	fmt.Println("testing", sudoku.Count)
+	var fields = [][]int{
+		{5, 3, 2, 1, 2, 2, 2, 2, 2},
+		{5, 3, 2, 1, 2, 2, 2, 2, 2},
+		{5, 3, 2, 1, 2, 2, 2, 2, 2},
+		{5, 3, 2, 1, 2, 2, 2, 2, 2},
+		{5, 3, 2, 1, 2, 2, 2, 2, 2},
+		{5, 3, 2, 1, 2, 2, 2, 2, 2},
+		{5, 3, 2, 1, 2, 2, 2, 2, 2},
+		{5, 3, 2, 1, 2, 2, 2, 2, 2},
+		{5, 3, 2, 1, 2, 2, 2, 2, 2},
+	}
+	sudoku, err := NewSudoku(fields)
+	if err != nil {
+		log.Fatal("error occured: ", err)
+	}
+	fmt.Println("this is sudoku setup", sudoku)
 }
