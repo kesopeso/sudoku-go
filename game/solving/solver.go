68ccc40a5121b8f9b5f092adccd7fa4fcbe68e63
Dorde Kesic <dordekesic.keso@gmail.com>
2025-07-27 16:58:43 +0200

    unimportant changes

--- game/solving/solver.go
+++ game/solving/solver.go
@@ -1,8 +1,8 @@
 package solving
 
-import "github.com/kesopeso/sudoku-go/game"
-
-// "math"
+import (
+	"github.com/kesopeso/sudoku-go/game"
+)
 
 type CellSolver interface {
 	SolveCell(s *game.Sudoku, row int, column int) []int
@@ -22,12 +22,4 @@ func NewSolver() Solver {
 	return Solver{cellSolvers: cellSolvers}
 }
 
-func (sudoku *Solver) Solve(s *game.Sudoku) {
-	for {
-		// run all cell solvers on each cell in parallel, get results from each, and use intersection to get possible results
-		// if there are cells that have only one value, set that value and run again
-		// if there are no cells with multiple values left, stop and return result
-		// otherwise run in a decision tree and select the winning result
-		// once is solved break
-	}
-}
+func (so *Solver) Solve(s *game.Sudoku) {}
--- game/solving/solver_test.go
+++ game/solving/solver_test.go
@@ -1,9 +1,27 @@
 package solving_test
 
-import "testing"
+import (
+	"testing"
+
+	"github.com/kesopeso/sudoku-go/game/solving"
+	"github.com/kesopeso/sudoku-go/game/testutil"
+)
 
 func TestSolveOneIteration(t *testing.T) {
 	t.Run("solve cell which gets result from the row solver", func(t *testing.T) {
+		sudoku := testutil.InitSudoku(t, [][]int{
+			{0, 0, 0, 0, 0, 0, 0, 0, 0},
+			{0, 0, 0, 0, 0, 0, 0, 0, 0},
+			{0, 0, 0, 0, 0, 0, 0, 0, 0},
+			{0, 0, 0, 0, 0, 0, 0, 0, 0},
+			{1, 2, 3, 4, 0, 6, 9, 7, 8},
+			{0, 0, 0, 0, 0, 0, 0, 0, 0},
+			{0, 0, 0, 0, 0, 0, 0, 0, 0},
+			{0, 0, 0, 0, 0, 0, 0, 0, 0},
+			{0, 0, 0, 0, 0, 0, 0, 0, 0},
+		})
+		solver := solving.NewSolver()
+		solver.Solve(sudoku)
 	})
 
 	t.Run("solve cell which gets result from the column solver", func(t *testing.T) {
